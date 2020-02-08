fb302995b40231115eaa2679117d9873b1113afc
Brian Fay <bcfay@wpi.edu>
2020-02-07 19:56:19 -0500

    Added Employee Class

--- src/Sales.java
+++ src/Sales.java
@@ -1,3 +1,25 @@
+import com.sun.security.ntlm.Client;
+import java.util.ArrayList;
+
+abstract class  Employee{
+
+  private ArrayList<Client> clients;
+
+  public boolean addClient(){
+    return false;
+  }
+
+  public ArrayList<Client> getClients(){
+    return null;
+  }
+}
+
+
+
+
+
+
+
 public class Sales {
 
 }
