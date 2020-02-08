ea10d52f7174de5d61d98302a8825df927d563ff
Brian Fay <bcfay@wpi.edu>
2020-02-07 21:59:03 -0500

    Added all getters and setters

--- src/Sales.java
+++ src/Sales.java
@@ -3,6 +3,16 @@ import java.util.ArrayList;
 
 class Client{
   int clientID;
+  Employee salesPerson;
+  String clientName;
+
+  public void setSalesPerson(Employee salesPerson) {
+    this.salesPerson = salesPerson;
+  }
+
+  public Employee getSalesPerson() {
+    return salesPerson;
+  }
 
   public void setClientID(int clientID) {
     this.clientID = clientID;
@@ -12,7 +22,6 @@ class Client{
     this.clientName = clientName;
   }
 
-  String clientName;
 
   public double totalSales(){
     return 0;
@@ -31,18 +40,53 @@ class Client{
   }
 }
 
+
+
+
 abstract class  Employee{
 
   private ArrayList<Client> clients;
 
-  public boolean addClient(){
-    return false;
+  public void setClients(ArrayList<Client> clients) {
+    this.clients = clients;
+  }
+
+  /**
+   * Adds new client to teh employees list of clients sorted by ID number
+   * @param newClient client to be added
+   * @return true if client is successfully added, false if ID number is not unique
+   */
+  public boolean addClient(Client newClient){
+
+    //link client to employee
+    newClient.setSalesPerson(this);
+
+    //sort by ID number
+    for(int i =0; i<this.clients.size();i++){
+
+      Client currentClient = this.clients.get(i);
+
+      if(newClient.getClientID()<(currentClient.getClientID())){
+        //adds client in location of first node it is smaller than
+        clients.add(i,newClient);
+        return true;
+      }
+      if(newClient.getClientID()==(currentClient.getClientID())){
+        //if ID number is not unique return false
+        return false;
+      }
+    }
+    //add client to the end if ID number is larger than all other ID numbers
+    clients.add(newClient);
+    return true;
   }
 
   public ArrayList<Client> getClients(){
     return null;
   }
 }
+
+
 
 class SalesManager extends Employee{
 
@@ -94,9 +138,10 @@ class SalesManager extends Employee{
   double bonus() {
     return 0;
   }
-
-
 }
+
+
+
 class SalesAssociate extends Employee{
 
   //fields
