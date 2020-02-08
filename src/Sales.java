149622d4c259923cc254eeb306778cde5f01bc91
Brian Fay <bcfay@wpi.edu>
2020-02-07 20:20:31 -0500

    Added all getters and setters

--- src/Sales.java
+++ src/Sales.java
@@ -1,54 +1,134 @@
 import java.util.ArrayList;
 
+
 class Client{
-    int clientID;
-    String clientName;
+  int clientID;
 
-    public double totalSales(){
-        return 0;
-    }
+  public void setClientID(int clientID) {
+    this.clientID = clientID;
+  }
 
-    public String toString(){
-        return null;
-    }
+  public void setClientName(String clientName) {
+    this.clientName = clientName;
+  }
+
+  String clientName;
+
+  public double totalSales(){
+    return 0;
+  }
+
+  public int getClientID() {
+    return clientID;
+  }
+
+  public String getClientName() {
+    return clientName;
+  }
+
+  public String toString(){
+    return null;
+  }
 }
 
 abstract class  Employee{
 
-    private ArrayList<com.sun.security.ntlm.Client> clients;
+  private ArrayList<Client> clients;
 
-    public boolean addClient(){
-        return false;
-    }
+  public boolean addClient(){
+    return false;
+  }
 
-    public ArrayList<com.sun.security.ntlm.Client> getClients(){
-        return null;
-    }
+  public ArrayList<Client> getClients(){
+    return null;
+  }
 }
 
 class SalesManager extends Employee{
-    String employeeName;
-    ArrayList<Employee> employees;
-    SalesManager manager;
-    long salesID;
 
-    public String toString(){
-        return null;
-    }
+  //fields
+  String employeeName;
+  ArrayList<Employee> employees;
+  SalesManager manager;
+  long salesID;
 
-    double bonus() {
-        return 0;
-    }
+  //setters
+  public void setEmployeeName(String employeeName) {
+    this.employeeName = employeeName;
+  }
 
-    class SalesAssociate extends Employee{
+  public void setEmployees(ArrayList<Employee> employees) {
+    this.employees = employees;
+  }
 
-      //fields
-      String employeeName;
-      SalesManager manager;
-      long salesID;
+  public void setManager(SalesManager manager) {
+    this.manager = manager;
+  }
 
-      private double bonus(){
-        return 0;
-      }
-    }
+  public void setSalesID(long salesID) {
+    this.salesID = salesID;
+  }
+  //getters
+  public String getEmployeeName() {
+    return employeeName;
+  }
+
+  public ArrayList<Employee> getEmployees() {
+    return employees;
+  }
+
+  public SalesManager getManager() {
+    return manager;
+  }
+
+  public long getSalesID() {
+    return salesID;
+  }
+
+
+
+  public String toString(){
+    return null;
+  }
+
+  double bonus() {
+    return 0;
+  }
+
+
+}
+class SalesAssociate extends Employee{
+
+  //fields
+  String employeeName;
+  SalesManager manager;
+  long salesID;
+
+  public void setEmployeeName(String employeeName) {
+    this.employeeName = employeeName;
+  }
+
+  public void setManager(SalesManager manager) {
+    this.manager = manager;
+  }
+
+  public void setSalesID(long salesID) {
+    this.salesID = salesID;
+  }
+
+  public String getEmployeeName() {
+    return employeeName;
+  }
+
+  public SalesManager getManager() {
+    return manager;
+  }
+
+  public long getSalesID() {
+    return salesID;
+  }
+
+  private double bonus(){
+    return 0;
+  }
 }
